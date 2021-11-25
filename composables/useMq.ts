05a9321fa982f9223652d8b8448b89bce75e9b7e
Issakov Ilya <mr.nonamerz08@yandex.ru>
2021-11-25 18:02:52 +0300

    del com

--- composables/useMq.ts
+++ composables/useMq.ts
@@ -4,46 +4,6 @@ import { useMq as Mq } from 'vue3-mq'
 export default function useMq() {
   const mq = Mq()
 
-  // const isMobile = computed(() => {
-  //   return mq.current === 'phone'
-  // })
-  //
-  // const isTablet = computed(() => {
-  //   return mq.current === 'tablet'
-  // })
-  //
-  // const isAir = computed(() => {
-  //   return mq.current === 'air'
-  // })
-  //
-  // const isDesktop = computed(() => {
-  //   return mq.current === 'air' || mq.current === 'lg' || mq.current === 'xl' || mq.current === 'dxl'
-  // })
-  //
-  // const isLg = computed(() => {
-  //   return mq.current === 'lg'
-  // })
-  //
-  // const isXl = computed(() => {
-  //   return mq.current === 'xl'
-  // })
-  //
-  // const is4xl = computed(() => {
-  //   return mq.current === 'dxl'
-  // })
-  //
-  // const isMobileOrTablet = computed(() => {
-  //   return isMobile.value || mq.current === 'tablet'
-  // })
-  //
-  // const isTabletOrDesktop = computed(() => {
-  //   return isTablet.value || isDesktop.value
-  // })
-  //
-  // const screen = computed(() => {
-  //   return mq.current
-  // })
-
   const isPhone = computed(() => {
     return ['phone'].includes(mq.current)
   })
